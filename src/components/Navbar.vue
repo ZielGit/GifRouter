b6346f968f96743c039b4613f2422a676caaede7
Ziel <frans_421_12@hotmail.com>
2022-11-08 21:01:54 -0500

    Proyecto Base

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <RouterLink class="navbar-brand" to="/">Home</RouterLink>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <RouterLink class="nav-link active" to="/">Home</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" to="/gifs">Gifs</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" to="/stickers">Stickers</RouterLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+import { RouterLink } from "vue-router";
+export default {
+    setup () {
+        return {}
+    }
+}
+</script>
