bdfaa944734dc11dc77039057b10044714ed2777
Ziel <frans_421_12@hotmail.com>
2022-11-09 21:08:57 -0500

    RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
-<script setup>
+<script>
+import { RouterView } from 'vue-router';
 import Navbar from './components/Navbar.vue';
 
+export default {
+  components: {
+    RouterView, Navbar
+  }
+};
 </script>
 
 <template>
   <Navbar />
 
+  <div class="container mt-3 border">
+    <RouterView />
+  </div>
 </template>
