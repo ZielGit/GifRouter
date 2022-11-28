9cccd9c7ca6a311a13e504653dd6f2c3443b0790
Ziel <frans_421_12@hotmail.com>
2022-11-27 21:23:29 -0500

    Diseño search gif

--- src/views/GifsView.vue
+++ src/views/GifsView.vue
@@ -2,10 +2,14 @@
     <div>
         <h1>Search Gif</h1>
         <div class="row">
-            <div v-for="gif in gifs" class="col-12 col-md-3">
-                <span :key="gif.id">
-                    {{ gif.title }}
-                </span>
+            <div v-for="gif in gifs" :key="gif.id" class="col-12 col-md-4 g-3">
+                <div class="card">
+                    <img class="card-img-top" :src="gif.images.original.url" alt="Image" height="250">
+                    <div class="card-body">
+                        <h4 class="card-title">{{ gif.title }}</h4>
+                        <a href="#" class="btn btn-primary">Download</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
