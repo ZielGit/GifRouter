c4c00971b24e2f98630734aac85a4a26ad3d1e23
Ziel <frans_421_12@hotmail.com>
2022-11-28 23:03:30 -0500

    lazy loading

--- src/views/GifsView.vue
+++ src/views/GifsView.vue
@@ -4,7 +4,13 @@
         <div class="row">
             <div v-for="gif in gifs" :key="gif.id" class="col-12 col-md-4 g-3">
                 <div class="card">
-                    <img class="card-img-top" :src="gif.images.original.url" alt="Image" height="250">
+                    <img
+                        class="card-img-top"
+                        :src="gif.images.original.url"
+                        :alt="gif.id"
+                        height="250"
+                        loading="lazy"
+                    >
                     <div class="card-body">
                         <h4 class="card-title">{{ gif.title }}</h4>
                         <a href="#" class="btn btn-primary">Download</a>
@@ -25,7 +31,7 @@ export default {
     },
     methods: {
         async getGifs() {
-            const res = await fetch("https://api.giphy.com/v1/gifs/search?api_key=IGvt90bSMcE0dhKhcT4uLHTycvybOecy&q=pikachu");
+            const res = await fetch("https://api.giphy.com/v1/gifs/search?api_key=IGvt90bSMcE0dhKhcT4uLHTycvybOecy&q=pikachu&limit=50");
             const { data } = await res.json();
 
             this.gifs = data;
